99a5a76ebc5d47d042043d6cecb2814de2d37616
ib-nosirov <ibrohim@nosirov.com>
2021-07-07 16:51:45 -0600

    model infrastructure

---
+++ reachingtaskdetector/model_base.py
@@ -0,0 +1,6 @@
+
+class ModelBase(object):
+  """A base class for all models used in modeling the reaching task."""
+
+  def __init__(self):
+    """Creates dictionaries for storing references to model parameters."""
