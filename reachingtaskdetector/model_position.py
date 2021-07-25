6d1dfc2c29d06b1dbcf4b360538f54645334c552
ib-nosirov <ibrohim@nosirov.com>
2021-07-25 10:52:06 -0600

    successfully define reach.

--- reachingtaskdetector/model_position.py
+++ reachingtaskdetector/model_position.py
@@ -1,20 +1,67 @@
 import numpy as np
 
-def reach(np_arr):
+def position_reach(np_arr):
   is_reaching = False
+  temp = 0
+  for i in range(np_arr.shape[0]-1):
+    if(np_arr[i] < 755 and not is_reaching):
+      temp = i
+      is_reaching = True
+      print('start' + str(convert_to_time(i)))
+        
+    if(is_reaching and (i - temp)>30):
+      print('end' + str(convert_to_time(i)))
+      is_reaching = False
+   
+  print(reach_count)
 
+#def classify_attempt(np_arr):
+  
+
+def position_reach_with_pellet(np_arr):
+  is_reaching = False
+  reach_count = 0
+  temp = 0
+  for i in range(np_arr.shape[0]-1):
+    if(np_arr[i,0] < 755 and not is_reaching and not np.isnan(np_arr[i,1])):
+      temp = i
+      is_reaching = True
+      print('start' + str(convert_to_time(i)))
+      reach_count = reach_count + 1
+        
+    if(is_reaching and (i - temp)>30):
+      print('end' + str(convert_to_time(i)))
+      is_reaching = False
+   
+  print(reach_count)
+
+def sliding_window(np_arr):
+  np_sums = np.empty(np_arr.shape[0] - 30, dtype=np.float32)
+  np_sums[:] = np.NaN
+  for i in range(np_arr.shape[0] - 30):
+    if(np_arr[i] < 810 and np_arr[i] > 740):
+      sum = 0
+      for j in range(30):
+        sum = sum + np_arr[i+j]
+      if(not np.isnan(sum)):
+        np_sums[i] = sum
+  return np_sums
+
+def velocity_reach(np_arr):
+  is_reaching = False
+  reach_count = 0
   for i in range(np_arr.shape[0] - 1):
     if(np_arr[i] < 810 and np_arr[i] > 740):
       temp = i+1
-      while(np_arr[i] == np.nan):
-        i = i + 1
-        print(i)
       velocity = (np_arr[i] - np_arr[temp])/(i - temp)
-      if(velocity < -50 and not is_reaching):
+      if(velocity < -10 and not is_reaching):
         is_reaching = True
-        print('reach ' + str(convert_to_time(i)))
+        reach_count = reach_count + 1
+        #print('reach ' + str(convert_to_time(i)))
       else:
         is_reaching = False
+
+  print(reach_count)
 
 def create_timestamps(data_frame):
   timestamps_list = []
@@ -41,10 +88,15 @@ def output_timestamps(timestamps_list):
       print("End Attempt: ", str(time[0]) + ":" + str(time[1]))
     i = i+1
 
-
 def convert_to_time(frame):
   time = float(frame / 59)
   seconds = float(time % 60)
   minutes = int(time / 60)
   return (minutes,seconds)
 
+def convert_windows_to_time(frame):
+  frame = frame + 20
+  time = float(frame / 59)
+  seconds = float(time % 60)
+  minutes = int(time / 60)
+  return (minutes,seconds)
--- reachingtaskdetector/plot_data.py
+++ reachingtaskdetector/plot_data.py
@@ -14,6 +14,33 @@ def make_xcoord_scatter(title, x_axis, y_axis, *args):
   # Make the plot.
   plt.show()
 
+def make_xcoord_line(title, x_axis, y_axis, *args):
+  plt.title(title)
+  plt.xlabel(x_axis)
+  plt.ylabel(y_axis)
+
+  # Stack scatterplots
+  for tuple in args:
+    t_minutes = convert_frames_to_minutes(tuple.shape[0])
+    plt.plot(t_minutes, tuple[:,0])
+
+  # Make the plot.
+  plt.show()
+
+
+def make_1d_scatter(title, x_axis, y_axis, *args):
+  plt.title(title)
+  plt.xlabel(x_axis)
+  plt.ylabel(y_axis)
+
+  # Stack scatterplots
+  for tuple in args:
+    t_minutes = convert_frames_to_minutes(tuple.shape[0])
+    plt.scatter(t_minutes, tuple[:], s=5)
+
+  # Make the plot.
+  plt.show()
+
 def convert_frames_to_minutes(num_frames):
   np_frames = np.arange(num_frames)
   return np_frames / 3600.
