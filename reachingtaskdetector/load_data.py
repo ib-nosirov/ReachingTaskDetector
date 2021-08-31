2c2a304d93c639997a0d42900fe6268c9005da47
ib-nosirov <ibrohim@nosirov.com>
2021-08-30 20:20:42 -0600

    maintainance

--- reachingtaskdetector/load_data.py
+++ reachingtaskdetector/load_data.py
@@ -1,51 +1,51 @@
 import numpy as np
 import pandas as pd
 def create_np_arr(data_path):
-  """Create a dictionary of NumPy arrays with labels corresponding to each
-     object marker.
-     The arrays contain three columns:
-       column 1: x_coord
-       column 2: y_coord
-       column 3: likelihood - lh
+    """Create a dictionary of NumPy arrays with labels corresponding to each
+         object marker.
+         The arrays contain three columns:
+             column 1: x_coord
+             column 2: y_coord
+             column 3: likelihood - lh
 
-  Args:
-    data_path: The full path of the .csv file containing object marker
-      positions for every frame of video.
-  """
-  # Read in data from path
-  data_frame = read_in_file(data_path)
+    Args:
+        data_path: The full path of the .csv file containing object marker
+            positions for every frame of video.
+    """
+    # Read in data from path
+    data_frame = read_in_file(data_path)
 
-  # Extract the labels of the object markers.
-  df_labels = get_labels(data_frame)
+    # Extract the labels of the object markers.
+    df_labels = get_labels(data_frame)
 
-  # Clean up extraneous labels.
-  data_frame = remove_labels(data_frame)
+    # Clean up extraneous labels.
+    data_frame = remove_labels(data_frame)
 
-  # Split and sort the array into corresponding to labels
-  numpy_df = data_frame.to_numpy(dtype = 'float32')
-  numpy_dict = create_np_dict(df_labels, numpy_df)
-  return numpy_dict
+    # Split and sort the array into corresponding to labels
+    numpy_df = data_frame.to_numpy(dtype = 'float32')
+    numpy_dict = create_np_dict(df_labels, numpy_df)
+    return numpy_dict
 
 def create_np_dict(labels_list, numpy_df):
-  numpy_dict = {}
-  for i in range(0, numpy_df.shape[1], 3):
-    j = int(i / 3)
-    numpy_dict[labels_list[j]] = numpy_df[:, i:i + 3]
-  return numpy_dict
+    numpy_dict = {}
+    for i in range(0, numpy_df.shape[1], 3):
+        j = int(i / 3)
+        numpy_dict[labels_list[j]] = numpy_df[:, i:i + 3]
+    return numpy_dict
 
 def remove_labels(data_frame):
-  data_frame = data_frame.iloc[2:, 1:]
-  data_frame = data_frame.reset_index(drop=True)
-  return data_frame
+    data_frame = data_frame.iloc[2:, 1:]
+    data_frame = data_frame.reset_index(drop=True)
+    return data_frame
 
 def get_labels(data_frame):
-  return [i for i in data_frame.iloc[0, 1::3]]
+    return [i for i in data_frame.iloc[0, 1::3]]
 
 def read_in_file(data_path):
-  return pd.read_csv(data_path)
+    return pd.read_csv(data_path)
 
 def apply_prob_filter(numpy_arr, threshold=.5):
-  for i in range(numpy_arr.shape[0]):
-    if(numpy_arr[i, 2] < threshold):
-      numpy_arr[i, 1] = np.nan 
-      numpy_arr[i, 0] = np.nan
+    for i in range(numpy_arr.shape[0]):
+        if(numpy_arr[i, 2] < threshold):
+            numpy_arr[i, 1] = np.nan 
+            numpy_arr[i, 0] = np.nan
--- reachingtaskdetector/model_position.py
+++ reachingtaskdetector/model_position.py
@@ -1,48 +1,47 @@
 import numpy as np
 
 def get_reaches(np_arr, np_pellet):
-  is_reaching = False
-  temp = 0
-  reach_list = []
-  for i in range(np_arr.shape[0]-1):
-    if(np_arr[i] < 765 and not is_reaching and not np.isnan(np_pellet[i])):
-      temp = i
-      is_reaching = True
-      reach_list.append((i, i))
+    is_reaching = False
+    temp = 0
+    reach_list = []
+    for i in range(np_arr.shape[0]-1):
+        if(np_arr[i] < 765 and not is_reaching and not np.isnan(np_pellet[i])):
+            temp = i
+            is_reaching = True
+            reach_list.append((i, i))
 
-    if(is_reaching):
-      if((i - temp)>30 or np_arr[i] > 765):
-        is_reaching = False
-        reach_list[-1] = (temp, i)
-  return reach_list
+        if(is_reaching):
+            if((i - temp)>30 or np_arr[i] > 765):
+                is_reaching = False
+                reach_list[-1] = (temp, i)
+    return reach_list
 
 def is_successful(np_snout_paw, end):
-  truth_value = False
-  count = 0
-  for i in range(90):
-    np_velocity = np_snout_paw[end+i+1] - np_snout_paw[end+i]
-    if(np_velocity < 1 and np_velocity > -1):
-      count = count + 1
-  if(count > 30):
-    truth_value = True
-  return truth_value
+    truth_value = False
+    count = 0
+    for i in range(90):
+        np_velocity = np_snout_paw[end+i+1] - np_snout_paw[end+i]
+        if(np_velocity < 1 and np_velocity > -1):
+            count = count + 1
+    if(count > 30):
+        truth_value = True
+    return truth_value
 
 def check_attempt(reach_list):
-  attempts_list = [(reach_list[0][0], reach_list[0][1], 1)]
-  for i in range(len(reach_list)-1):
-    if(reach_list[i+1][0]-reach_list[i][1] < 60):
-      if(reach_list[i+1][0]-attempts_list[-1][1] < 60):
-        attempts_list[-1] = (attempts_list[-1][0], reach_list[i+1][1],
-                             attempts_list[-1][2]+1)
-      else:
-        attempts_list.append((reach_list[i][0], reach_list[i+1][1], 2))
-    else:
-      attempts_list.append((reach_list[i+1][0], reach_list[i+1][1], 1))
-
-  return attempts_list
+    attempts_list = [(reach_list[0][0], reach_list[0][1], 1)]
+    for i in range(len(reach_list)-1):
+        if(reach_list[i+1][0]-reach_list[i][1] < 60):
+            if(reach_list[i+1][0]-attempts_list[-1][1] < 60):
+                attempts_list[-1] = (attempts_list[-1][0], reach_list[i+1][1],
+                                     attempts_list[-1][2]+1)
+            else:
+                attempts_list.append((reach_list[i][0], reach_list[i+1][1], 2))
+        else:
+            attempts_list.append((reach_list[i+1][0], reach_list[i+1][1], 1))
+    return attempts_list
 
 def convert_to_time(frame):
-  time = float(frame / 60)
-  seconds = float(time % 60)
-  minutes = int(time / 60)
-  return (minutes,seconds)
+    time = float(frame / 60)
+    seconds = float(time % 60)
+    minutes = int(time / 60)
+    return (minutes,seconds)
